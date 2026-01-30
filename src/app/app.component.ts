b3e92aa5f0352b45665bcea07342678ebe2f520e
Yohon Jairo Bravo <yjairobravo@gmail.com>
2026-01-29 20:08:13 -0500

    :sparkles: migrate to angular 20 ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { PhaserGame } from "./phaser-game/game/phaser-game.component";
 
 @Component({
-    selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, RouterLink, PhaserGame],
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  imports: [CommonModule, RouterOutlet, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit
 {
- public collapseSidebar = false;
+  public collapseSidebar = false;
 
-    ngAfterViewInit(){
-      console.warn("After View Angular component");
-    }
+  ngAfterViewInit()
+  {
+    console.warn("After View Angular component");
+  }
 
-  
+
 }
